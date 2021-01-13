f942c20e3b5b1202069c273851810a7a5819f439
mdrdani <sudokill3004@gmail.com>
2021-01-13 16:41:21 +0700

    Make folder Trait and make trait UsesUuid

---
+++ app/Traits/UsesUuid.php
@@ -0,0 +1,37 @@
+<?php 
+
+namespace App\Traits;
+
+trait UsesUuid{
+    
+    public static function bootUsesUuid()
+    {
+        static::creating(function ($model) {
+            if ( ! $model->getKey()) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+
+    /**
+     * Get the value indicating whether the IDs are incrementing.
+     *
+     * @return bool
+     */
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    /**
+     * Get the auto-incrementing key type.
+     *
+     * @return string
+     */
+    public function getKeyType()
+    {
+        return 'string';
+    }
+}
+
+?>

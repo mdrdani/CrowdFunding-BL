a64c5404a67c1199a74acc8a7cca2527b3584ec4
mdrdani <sudokill3004@gmail.com>
2021-01-20 22:42:00 +0700

    Queue Event Listener Mail Laravel

--- resources/views/emails/otp_code.blade.php
+++ resources/views/emails/otp_code.blade.php
@@ -169,17 +169,6 @@
                 </table>
             </td>
         </tr>
-        <tr>
-            <td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">
-                <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-                    <tr>
-                        <td bgcolor="#f4f4f4" align="left" style="padding: 0px 30px 30px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 18px;"> <br>
-                            <p style="margin: 0;">If these emails get annoying, please feel free to <a href="#" target="_blank" style="color: #111111; font-weight: 700;">unsubscribe</a>.</p>
-                        </td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
     </table>
 </body>
 
